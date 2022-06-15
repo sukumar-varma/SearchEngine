2ce76c6964e806413b6c14fefbff8b525d4917ff
Sukumar Varma <s.varmagt@gmail.com>
2022-06-14 20:00:17 -0500

    Added :: web spider tocrawl data from UTD

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Hello, team!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
